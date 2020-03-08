5b036df30e93a94334cfaa1aa9fd8d6eb68f7920
Petros9 <58508596+Petros9@users.noreply.github.com>
2020-03-08 22:19:49 +0100

    Add files via upload

--- zadanie_2/main.c
+++ zadanie_2/main.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mylib.c"
-
+#include <unistd.h>
 #include <time.h>
 #include <sys/times.h>
-#include <unistd.h>
 
 array_of_blocks *exec_initialise_array(char *size_as_string)
 {
@@ -23,13 +22,9 @@ void exec_make_pairs(char *files_as_chars, array_of_blocks *main_array)
     do_diff(make_pairs(files_str), main_array);
 }
 
-void exec_create_block(char *file_name, array_of_blocks *main_array)
+int exec_operationss_number(array_of_blocks *main_array, char *index_as_str)
 {
-    return create_block(file_name, main_array);
-}
-
-int exec_operationss_number(array_of_blocks *main_array, int index)
-{
+    int index = atoi(index_as_str);
     return operationss_number(main_array,index);
 }
 
@@ -73,24 +68,55 @@ int main(int args_num, char *args[])
         tms_time[j] = (struct tms *)calloc(1, sizeof(struct tms *));
     }
 
+while (i < args_num)
+    {
+        char *command = args[i];
 
-
-        // START TIME
+        // początek mierzenia czasu
         real_time[0] = times(tms_time[0]);
 
-        main_array = exec_initialise_array("3");
-        
-        exec_make_pairs("lamba.txt:beta.txt a.txt:b.txt c.txt:cprim.txt", main_array); 
-        //exec_delete_operation(main_array,"0","1");
-
-
+        if (strcmp(command, "initialise_array") == 0)
+        {
+            main_array = exec_initialise_array(args[i + 1]);
+            i += 2;
+        }
+        else if (strcmp(command, "make_pairs") == 0)
+        {
+            exec_make_pairs(args[i + 1], main_array);
+            i += 2;
+        }
+        else if (strcmp(command, "operationss_number") == 0)
+        {
+            int number = exec_operationss_number(main_array, args[i+1]);
+            i += 2;
+        }
+        else if (strcmp(command, "delete_block") == 0)
+        {
+            exec_delete_block(main_array,args[i+1]);
+            i += 2;
+        }
+        else if (strcmp(command, "delete_operation") == 0)
+        {
+            exec_delete_operation(main_array,args[i + 1], args[i + 2]);
+            i += 3;
+        }
+        else
+        {
+            i++;
+        }
         real_time[1] = times(tms_time[1]);
-        //printf("[REAL_TIME] Executing action %s took %fs\n", command, calculate_time(real_time[0], real_time[1]));
-        //printf("[USER_TIME] Executing action %s took %fs\n", command, calculate_time(tms_time[0]->tms_utime, tms_time[1]->tms_utime));
-        //printf("[SYSTEM_TIME] Executing action %s took %fs\n", command, calculate_time(tms_time[0]->tms_stime, tms_time[1]->tms_stime));
+        printf("[REAL_TIME] Executing action %s took %fs\n", command, calculate_time(real_time[0], real_time[1]));
+        printf("[USER_TIME] Executing action %s took %fs\n", command, calculate_time(tms_time[0]->tms_utime, tms_time[1]->tms_utime));
+        printf("[SYSTEM_TIME] Executing action %s took %fs\n", command, calculate_time(tms_time[0]->tms_stime, tms_time[1]->tms_stime));
+    }
     real_time_program[1] = times(program_time[1]);
     printf("[REAL_TIME] Executing main.c took %fs\n", calculate_time(real_time_program[0], real_time[1]));
     printf("[USER_TIME] Executing main.c took %fs\n", calculate_time(program_time[0]->tms_utime, program_time[1]->tms_utime));
     printf("[SYSTEM_TIME] Executing main.c took %fs\n", calculate_time(program_time[0]->tms_stime, program_time[1]->tms_stime));
     return 0;
 }
+        //real_time[0] = times(tms_time[0]);
+
+        //main_array = exec_initialise_array("3");
+        //exec_make_pairs("lamba.txt:beta.txt a.txt:b.txt c.txt:cprim.txt", main_array); 
+        //exec_delete_operation(main_array,"0","1");
