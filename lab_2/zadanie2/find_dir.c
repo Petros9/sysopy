3e5a595e6a8cb607d3fef0a90c2b9320199bb18c
Petros9 <58508596+Petros9@users.noreply.github.com>
2020-03-14 21:35:06 +0100

    Add files via upload

--- lab_2/zadanie2/find_dir.c
+++ lab_2/zadanie2/find_dir.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <time.h>
 // stat, fstat, lstat - pobieranie stanu pliku
 
 
@@ -27,7 +28,7 @@ char* file_type(mode_t mode) { //ok
 void print_file_info(char* PATH, struct stat* info){ //ok
     printf("FILE PATH: %s \n", PATH); //ścieżka bezwględna
     printf("LINKED FILES NUMBER: %lu \n", info ->st_nlink); //liczba dowiązań
-    prinf("FILE TYPE: %s \n", file_type(info->st_mode)); // rodzaj pliku
+    printf("FILE TYPE: %s \n", file_type(info->st_mode)); // rodzaj pliku
     printf ("FILE SIZE %ld BYTES\n", info->st_size); //rozmiar w bajtach
 
     char access_date[24];
@@ -35,42 +36,173 @@ void print_file_info(char* PATH, struct stat* info){ //ok
     printf("\t LAST ACCESS DATE: %s\n", access_date); // data ostatniego dostępu
 
     char modification_date[24];
-    strftime(modification_date, sizeof(modification_date), "%d %m %Y %H:%M", localtime(&stats->st_mtime));
+    strftime(modification_date, sizeof(modification_date), "%d %m %Y %H:%M", localtime(&info->st_mtime));
     printf("\t LAST MODIFICATION DATE: %s\n", modification_date); //data ostatniej modyfikacji
 }
-void mtime(char* PATH){ //dane były obliczane przez ostatnie n dób
-
-}
-
-}
-
-void atime(){
-
-} 
-
-void maxdepth(char* PATH, int curr_depth){
- 
+int day_diff(time_t date) {
+    return difftime(time(NULL), date) / 86400;
 }
 
 
-
-
-void print_linked_files(char* PATH, char* command, int depth){
-
-    if(strcmp(command, "maxdepth") == 0){
-        maxdepth(PATH, depth);
+int m_time_correct(const struct stat* stats, char* command) { //data ostatniej modyfikacji
+    if(command == NULL){
+        return 1;
     }
-    else if(strcmp(command, "atime") == 0){
-        
-    }
-    else if(strcmp(command. "mtime") == 0){
-        
-    }
-    
 
+    int diff = day_diff(stats->st_mtime);
+    if(strcmp(command, "+") == 0){
+        char str_number[strlen(command)-1];
+        for(int i = 1; i<strlen(command);i++){
+            str_number[i-1] = command[i];
+        }
+        int number = atoi(str_number);
+        if(diff < number)
+        {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+    else if(strcmp(command, "-") == 0){
+        char str_number[strlen(command)-1];
+        for(int i = 1; i<strlen(command);i++){
+            str_number[i-1] = command[i];
+        }
+        int number = atoi(str_number); 
+        if(diff > number)
+        {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    else {
+        int number = atoi(command);
+        if(number == diff){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
 }
 
+int a_time_correct(const struct stat* stats, char* command){ //data ostatniego dostępu
+    if(command == NULL){
+        return 1;
+    }
+   int diff = day_diff(stats->st_atime);
+    if(strcmp(command, "+") == 0){
+        char str_number[strlen(command)-1];
+        for(int i = 1; i<strlen(command);i++){
+            str_number[i-1] = command[i];
+        }
+        int number = atoi(str_number);
+        if(diff < number)
+        {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    else if(strcmp(command, "-") == 0){
+        char str_number[strlen(command)-1];
+        for(int i = 1; i<strlen(command);i++){
+            str_number[i-1] = command[i];
+        }
+        int number = atoi(str_number); 
+        if(diff > number)
+        {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
 
-int main(void){
+    else {
+        int number = atoi(command);
+        if(number == diff){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
 
+void find_function(char* PATH, int curr_depth, int max_depth, char* a_command, char* m_command){ // compiluje sie
+    if(PATH == NULL){
+        return;
+    }
+    if(curr_depth == max_depth){
+        return;
+    }
+    DIR* dirent = opendir(PATH);
+
+    if(dirent == NULL){
+        printf("%s", "That dirent is empty");
+        exit(1);
+    }
+
+    struct dirent* sub_files;
+    char sub_PATH[100];
+    while((sub_files = readdir(dirent))!=NULL){
+
+        strcpy(sub_PATH, PATH);
+        strcat(sub_PATH, "/");
+        strcat(sub_PATH,sub_files -> d_name);
+
+        struct stat* path_stats = (struct stat*)malloc(sizeof(struct stat));
+        lstat(sub_PATH, path_stats);
+
+        if (S_ISDIR(path_stats->st_mode))
+        {
+            if (strcmp(sub_files->d_name, ".") == 0 || strcmp(sub_files->d_name, "..") == 0)
+            {
+                continue;
+            }
+            find_function(sub_PATH, curr_depth + 1, max_depth, a_command, m_command);
+        }
+
+        int m = m_time_correct(path_stats, m_command);
+        int a = a_time_correct(path_stats, a_command);
+        if(m+a == 2){
+            print_file_info(sub_PATH, path_stats);
+        }
+    }
+
+    closedir(dirent);
+}
+
+int main(int arg_num, char* args[]){
+    int i = 2;
+    int max_depth = -1;
+    char* a = NULL;
+    char* m = NULL;
+    char* PATH = args[1];
+    while(i<arg_num){
+        if(strcmp(args[i], "maxdepth")==0){
+            max_depth = atoi(args[i+1]);
+            i = i+2;
+        }
+        else if(strcmp(args[i],"mtime")==0){
+            m = args[i+1];
+            i = i+2;
+        }
+        else if(strcmp(args[i], "atime")==0){
+            a = args[i+1];
+            i = i+2;
+        }
+        else{
+            i++;
+        }
+    }
+    find_function(PATH,0,max_depth,a,m);
 }
